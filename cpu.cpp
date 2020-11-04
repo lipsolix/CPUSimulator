aeccae98de092051e9edde1691131c1774a71c56
lipsolix <oligli@yandex.ru>
2020-11-04 16:16:18 +0300

    WOW

--- cpu.cpp
+++ cpu.cpp
@@ -37,25 +37,65 @@ void NOT(int dst, int src) {
     MEMORY[dst] = ~MEMORY[dst];
 }
 
+void RET(int dst, int src) {
+    if (dst == 80) {
+        cout << MEMORY[0] << endl;
+        return;
+    }
+}
+
 using func_t = void (*)(int, int);
 func_t OP[] = {NOP, MOV, ADD, SUB, NOT, OR, AND, XOR, RET};
 
-CMD PROG[] = {(2 << 16) | (6 << 8) | 8};
+
+void DOOP(CMD cmd) {
+    DW R = cmd % 256;
+    cmd /= 256;
+
+    DW L = cmd % 256;
+    cmd /= 256;
+
+    DW PROPS = cmd % 256;
+    cmd /= 256;
+
+    DW OPERATOR = cmd % 256;
+    
+    cout << "-" << OPERATOR << " " << L << " " << R << endl;
+    
+
+    if (PROPS % 16 > 0) 
+        R = MEMORY[R];
+
+    PROPS /= 16;
+
+    if (PROPS > 0) 
+        L = MEMORY[L];
+
+    cout << "+" << OPERATOR << " " << L << " " << R << endl;
+
+
+    OP[OPERATOR](L, R);
+}
+
+CMD createCmd(int OPERATOR, bool isPtrL, bool isPtrR, int L, int R) {
+    CMD tmp = (OPERATOR << 24) | (isPtrL << 20) | (isPtrR << 16) | (L << 8) | R;
+    cout << tmp << endl;
+    return tmp;
+}
+
+CMD PROG[] = {
+    createCmd(1, 1, 0, 6, 8),
+    createCmd(1, 0, 1, 0, 6),
+    createCmd(8, 0, 0, 80, 0)
+    };
 
 int main() {
-    MEMORY[6] = 6;
+    MEMORY[6] = 10;
     MEMORY[8] = 8;
-    int n = 1;
+    int n = 3;
     for (int i = 0; i < n; i++) {
-        DW R = PROG[i] % (2 << 8);
-        PROG[i] >>= 8;
-        DW L = PROG[i] % (2 << 8);
-        PROG[i] >>= 8;
-        DW OPERATOR = PROG[i];
-        cout << OPERATOR << " " << L << " " << R << endl;
-        OP[OPERATOR](L, R);
+        DOOP(PROG[i]);
     }
-    cout << MEMORY[6] << endl;
     
 
     return 0;
